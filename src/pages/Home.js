a2140e52f4c8318c675bbb1386c12483cfb0dda9
Waly FALL <2atelecompwf@gmail.com>
2025-12-14 22:39:17 +0000

    deployed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,40 @@
 import React from "react";
+import Slider from "react-slick";
+import "../style/home.css";
 
 const Home = () => {
-  return(
-    <div className="">
-      <div className="div-home">
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+  };
+  return(
+
+    <div className="">
+        <div className="div-home">
+
+          <Slider {...settings}>
+
+            <div>
+              <img src="/img/slide1.jpg" className="slide-img" />
+            </div>
+
+            <div>
+              <img src="/img/slide2.jpg" className="slide-img" />
+            </div>
+
+            <div>
+              <img src="/img/slide3.jpg" className="slide-img" />
+            </div>
+
+          </Slider>
+  
+
+        </div>
       </div>
-    </div>
 
   );
 
