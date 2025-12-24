5df0e137fb443a47665ea790b2abcbefa3caf314
Waly FALL <2atelecompwf@gmail.com>
2025-12-24 11:02:48 +0000

    deployed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Slider from "react-slick";
+//import Slider from "react-slick";
 import "../style/home.css";
-import image1 from "../assets/image1.jpg";
-import photo1 from "../assets/photo1.jpeg";
-import pwf from "../assets/pwf.jpg";
+//import image1 from "../assets/image1.jpg";
+//import photo1 from "../assets/photo1.jpeg";
+//import pwf from "../assets/pwf.jpg";
 
 const Home = () => {
 
