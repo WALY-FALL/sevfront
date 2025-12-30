1c4d6c57612bde7dedc1c05044a59a4bff269330
Waly FALL <2atelecompwf@gmail.com>
2025-12-30 17:19:51 +0000

    deployed

--- src/components/Fouteur.jsx
+++ src/components/Fouteur.jsx
@@ -4,7 +4,9 @@ const Fouteur = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-8">
       <div className="container-fouteur">
-        <p>&copy; {new Date().getFullYear()} 2atelecom. tout droit réservé.</p>
+        <p className="fouteur1">&copy; {new Date().getFullYear()} 2atelecom. tout droit réservé.</p>
+        <p className="fouteur2">Contacts: 77 515 27 49/ 76 722 77 77</p>
+        <p className="fouteur3">Email: 2atelecompwf@gmail.com</p>
       </div>
     </footer>
   );
